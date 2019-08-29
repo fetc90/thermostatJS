4834c59fca1871424f0240423869859425e194d0
Francesca Chater <francescachater@gmail.com>
2019-08-29 12:31:58 +0100

    Implement powersave mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,20 +8,34 @@ describe('Thermostat',function(){
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it('has a minimum of 10 degrees',function(){
-    for (i = 0; i < 12; i++ ) {
+  it('has a minimum temperature of 10 degrees',function(){
+    for (i = 0; i < 12; i++) {
       thermostat.decreaseTemperature();
     }
     expect(thermostat.temperature).toEqual(10);
-  });
-  
+  })
 
   describe('#increaseTemperature',function(){
-    it('increments the temperature by 1',function(){
+    it('increases the temperature by 1',function(){
       thermostat.increaseTemperature();
       expect(thermostat.temperature).toEqual(21);
-    });
-    
+    })
+
+      it('cannot increase temperature > 25 if power saving mode is on',function(){
+        for (i = 0; i < 7; i++) {
+        thermostat.increaseTemperature();
+        }
+        expect(thermostat.temperature).toEqual(25);
+      });
+
+      it('cannot increase temperature > 32 if power saving mode is off',function(){
+        thermostat.switchPower();
+        for (i = 0; i < 16; i++){
+        thermostat.increaseTemperature();
+        }
+        expect(thermostat.temperature).toEqual(32);
+      })
+      
   });
 
   describe('#decreaseTemperature',function(){
@@ -29,9 +43,15 @@ describe('Thermostat',function(){
       thermostat.decreaseTemperature();
       expect(thermostat.temperature).toEqual(19);
     });
+  });
+
+  describe('#SwitchPower',function(){
+    it('changes the powersaving mode',function(){
+      thermostat.switchPower();
+      expect(thermostat.powerSave).toEqual(false);
+    });
     
   });
   
-  
+   
 });
-
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,10 +1,21 @@
 function Thermostat(){
-  this.temperature = 20
-  this.minimumTemperature = 10
+  this.temperature = 20;
+  this.minimumTemperature = 10;
+  this.maximumTemperature = 32
+  this.powerSave = true;
+  this.powerSaveMax = 25
+
 }
 
 Thermostat.prototype.increaseTemperature = function(){
-  this.temperature += 1
+  if (this.powerSave && this.temperature === this.powerSaveMax){
+    return this.temperature
+  } else if (this.temperature === this.maximumTemperature){
+    return this.temperature
+  } else {
+    this.temperature += 1
+  }
+  
 };
 
 Thermostat.prototype.decreaseTemperature = function(){
@@ -15,5 +26,8 @@ Thermostat.prototype.decreaseTemperature = function(){
   } else {
     this.temperature -= 1
   }
-  
+};
+
+Thermostat.prototype.switchPower = function(){
+  this.powerSave = !this.powerSave
 };
